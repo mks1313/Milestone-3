0adfbb629f54420efd60881f85050ecd9369b225
mks1313 <mg.marinov@gmx.es>
2025-01-29 15:25:36 +0100

    .h optimizado

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 14:28:09 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/01/24 15:27:42 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/01/29 15:22:37 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,8 @@ typedef struct s_filo
 	pthread_t		thread;// Hilo del filósofo
 	int				id;// Identificador del filósofo
 	int				meals_done;// Número de comidas realizadas
-	int				eating;// Indicador de si está comiendo
+	bool			is_eating;// Indicador de si está comiendo
 	size_t			last_meal;// Tiempo de la última comida
-	size_t			time_start;// Tiempo de inicio del programa
 	pthread_mutex_t	*r_fork;// Puntero al tenedor a la derecha
 	pthread_mutex_t	*l_fork;// Puntero al tenedor a la izquierda
 	struct s_dta	*dta; //Puntero a la estructura de datos
@@ -37,18 +36,30 @@ typedef struct s_dta
 	pthread_mutex_t	dead_lock; // Mutex para proteger dead_flag
 	pthread_mutex_t	meal_lock; // Mutex para proteger el acceso a las comidas
 	pthread_mutex_t	write_lock;// Mutex para controlar la salida de datos
-	t_filo			**filos; // Array de filósofos
+	t_filo			*filos; // Array de filósofos
 	int				dead_flag; // Indicador de si algún filósofo ha muerto
 	size_t			tto_die;// Tiempo máximo sin comer antes de morir
 	size_t			tto_eat;// Tiempo que toma comer
 	size_t			tto_sleep;// Tiempo que toma dormir
 	int				n_filos;// Número total de filósofos
-	int				n_tto_eat;// Núm de veces que cada filósofo debe comer
+	int				n_meals;// Núm de veces que cada filósofo debe comer
 }					t_dta;
 
+// Utils
 int			ft_atoi(char *s);
-long long	get_time(void);
-int			check_input(char *arg);
+long long	curr_time(void);
+void		sleep_precise(long mls);
+
+// Inicialization & cleaning
 void		init_forks(pthread_mutex_t *forks, int n_filos);
+void		init_program(t_dta *dta, pthread_mutex_t *forks, char **argv);
+void		wash(t_filo *filos, int n_filos, tda *dta, pthread_mutex_t *forks);
+
+// Rutines of the Philos
+void		create_filo_threads(t_filo *filo, int n_filos);
+void		*filo_routine(void *arg);
+void		eat(t_filo *filo);
+void		think(t_filo *filo);
+void		sleep_filo(t_filo *filo);
 
 #endif
